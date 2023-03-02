9554720c78c57655b7855e83fda24f7059746edb
Grover Taboada <grovertabu@gmail.com>
2023-03-02 01:50:39 -0400

    cambios

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,61 @@ import moment from "moment";
 moment.locale("es");
 
 
-let fechaInicio = moment('2022-02-10T04:00:00.000Z'); // '01/02/2023'
-let fechaFin = moment('2023-03-10T04:00:00.000Z'); // '30/04/2023'
+function mesesInYear(year: number, mes: number, dias: number) {
+  
+  let typeYear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 366 : 365;
+  
+  const daysInMonth: {[key: string]: number} = {};
+
+// Itera sobre cada mes y almacena la cantidad de días en el objeto
+  for (let month = 1; month <= 12; month++) {
+    let mes:any = month < 10 ? `0${month}` : month;
+    const days = moment(`${year}-${mes}-01`).daysInMonth();
+    daysInMonth[month] = days;
+  }
+
+  if(dias == 31){
+    daysInMonth[mes] = 1;
+  }
+  if((typeYear == 366 && mes == 2 && dias == 29) ){
+    daysInMonth[mes] = 1;
+  }   
+  if(typeYear == 365 && mes == 2 && dias == 28){
+    daysInMonth[mes] = 1;
+  }
+  if(dias==30){
+    daysInMonth[mes] = 1;
+  }
+  if(dias< 30 && mes != 2){
+    daysInMonth[mes] = dias/30;
+  }
+  return (daysInMonth[mes]);
+}
+function mesesInYearStart(year: number, mes: number, dias: number) {
+  
+  let typeYear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 366 : 365;
+  
+  const daysInMonth: {[key: string]: number} = {};
+
+// Itera sobre cada mes y almacena la cantidad de días en el objeto
+  for (let month = 1; month <= 12; month++) {
+    let mes:any = month < 10 ? `0${month}` : month;
+    const days = moment(`${year}-${mes}-01`).daysInMonth();
+    daysInMonth[month] = days;
+  }
+
+  if(dias == 1){
+    daysInMonth[mes] = 1;
+  }
+  if(dias!=1){
+    
+    daysInMonth[mes] = ((30-dias)+1)/30;
+  }
+  return (daysInMonth[mes]);
+}
+
+let fechaInicio = moment('2022-12-31T04:00:00.000Z'); // '01/02/2023'
+let fechaFin = moment('2023-12-31T04:00:00.000Z'); // '30/04/2023'
 
 
 function calcularImporteTotal(fInicio:Date, fFin:Date, dias_pagados_mes:number){
@@ -22,20 +75,11 @@ function calcularImporteTotal(fInicio:Date, fFin:Date, dias_pagados_mes:number){
     let startYear = fechaInicio.year();
     let endYear = fechaFin.year();
 
+    let dias_inicio_contrato = mesesInYearStart(startYear, startMonth, startDay);
+    let dias_fin_contrato = mesesInYear(endYear, endMonth, endDay);
+
     let daysinMonthFechaInicio = fechaInicio.daysInMonth();
     let daysinMonthFechaFin = fechaFin.daysInMonth();
-
-    console.table({ 
-        anioInicio:startYear, 
-        anioFin :endYear, 
-        mesInicio:startMonth, 
-        mesFin:endMonth, 
-        diaInicio:startDay, 
-        diaFin:endDay,
-        diasMesInicio:daysinMonthFechaInicio,
-        diasMesFin:daysinMonthFechaFin
-    });
-
     
 
     const diffDays = fechaFin.diff(fechaInicio, 'days');
@@ -46,9 +90,19 @@ function calcularImporteTotal(fInicio:Date, fFin:Date, dias_pagados_mes:number){
     const diffMonths = Math.floor(diffDays / monthDays);
     const diffYears = Math.floor(diffDays / yearDays);
 
-    const months = diffMonths % 12;
+    let months = diffMonths % 12;
     const days = diffDays - diffMonths * monthDays - diffYears * (yearDays - monthDays);
 
+    if(dias_inicio_contrato != 1){
+      months = months - 1;
+      months= months + dias_inicio_contrato;
+    }
+    if(dias_fin_contrato != 1){
+      months = months - 1;
+      months= months + dias_fin_contrato;
+    }
+    console.log('months', months);
+    
     // console.log(`${diffYears} años, ${months} meses y ${days} días`);
     let factor_calculo = dias_pagados_mes == 30 ? months+1 : months + dias_pagados_mes / 30;
     // console.log('factor_calculo', factor_calculo);
@@ -59,12 +113,13 @@ function calcularImporteTotal(fInicio:Date, fFin:Date, dias_pagados_mes:number){
         months: months,
         days: days,
         factor_calculo: factor_calculo,
+        sueldo: 1000*factor_calculo
     });
     
 
     return factor_calculo;
   }
-//   calcularImporteTotal(fechaInicio.toDate(), fechaFin.toDate(), 30);
+  calcularImporteTotal(fechaInicio.toDate(), fechaFin.toDate(), 30);
 
 
   function calcularImporteTotal2() {
@@ -97,31 +152,7 @@ function calcularImporteTotal(fInicio:Date, fFin:Date, dias_pagados_mes:number){
 //   calcularImporteTotal2();
 
 
-function mesesInYear(year: number, mes: number, dias: number) {
-  let typeYear = (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ? 366 : 365;
-  
-  const daysInMonth: {[key: string]: number} = {};
 
-// Itera sobre cada mes y almacena la cantidad de días en el objeto
-  for (let month = 1; month <= 12; month++) {
-    let mes:any = month < 10 ? `0${month}` : month;
-    const days = moment(`${year}-${mes}-01`).daysInMonth();
-    daysInMonth[month] = days;
-  }
-
-  if(dias == 31){
-    daysInMonth[mes] = 30;
-  }
-  else{
-    daysInMonth[mes] = (30-dias)+1;
-  }
-  if((typeYear == 366 && mes == 2 && dias == 29) || (typeYear == 365 && mes == 2 && dias == 28)){
-    daysInMonth[2] = 30;
-  }
-
-  return (daysInMonth[mes]/30).toFixed(1) ;
-}
-console.log(mesesInYear(2023, 3,21));
 
 
 function salario(){
